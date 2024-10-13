79a8114b6b571fe0ba0764203b7b7d1cf23dba15
Andrii Shandryk <shandrykjj512@gmail.com>
2024-10-13 15:42:55 +0300

    m_s

--- rustlings/exercises/move_semantics/move_semantics4.rs
+++ rustlings/exercises/move_semantics/move_semantics4.rs
@@ -4,7 +4,7 @@
 // freshly created vector from fill_vec to its caller.
 // Execute `rustlings hint move_semantics4` for hints!
 
-// I AM NOT DONE
+// I AM DONE
 
 fn main() {
     let vec0 = Vec::new();
@@ -19,8 +19,8 @@ fn main() {
 }
 
 // `fill_vec()` no longer takes `vec: Vec<i32>` as argument
-fn fill_vec() -> Vec<i32> {
-    let mut vec = vec;
+fn fill_vec(vec0: Vec<i32>) -> Vec<i32> {
+    let mut vec = vec0;
 
     vec.push(22);
     vec.push(44);
